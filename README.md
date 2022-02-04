b1189d5a615ab980d574b190d495ef513a488a0e
Zo-Bro-23 <zohan.subhash@gmail.com>
2022-02-04 18:56:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# Writing-Stats
+This is a simple package to get stats (such as word count, paragraph density, etc) from a TXT file.
+```js
+const writingStats = require('writing-stats')
+
+console.log(writingStats('essay.txt'))
+```
+
+```writingStatus(file, lineCount, aboveAverageBuffer, belowAverageBuffer, sentenceEndCharacters, paragraphEndCharacter)```
+
+```file - Filepath to pass to fs module```
+
+```lineCount - Number of \n characters per paragraph```
+
+```aboveAverageBuffer - Number of words (more than the average sentence length) needed to count as an "Above Average" sentence```
+
+```belowAverageBuffer - Number of words (less than the average sentence length) to count as a "Below Average" sentence```
+
+```sentenceEndCharacters - List of characters (as Array) that mark the end of a sentence. For example, [".", "?", "!"]```
+
+```paragraphEndCharacter - Character that marks the end of a paragraph - Newline character```
+
+## Result
+```json
+{
+  wordCount: 774,
+  characterCount: 3499,
+  rawCharacterCount: 4439,
+  sentenceCount: 57,
+  paragraphCount: 12,
+  longestSentence: 35,
+  shortestSentence: 1,
+  wordCountsPerSentence: [
+     6, 11,  9,  5, 13, 16,  2,  9, 14, 11, 11, 11,
+    11, 12, 19,  9, 12, 16, 12,  9, 12, 14,  9, 13,
+    19,  7, 16, 19, 10, 11, 35, 14, 25, 24,  1,  5,
+    12,  8, 14, 12, 16,  8, 10, 27, 23, 24, 19,  9,
+    25, 19, 18, 19, 13, 17, 10,  3, 16
+  ],
+  longestParagraph: 8,
+  shortestParagraph: 3,
+  sentenceCountsPerParagraph: [
+    6, 4, 4, 4, 8,
+    4, 3, 5, 6, 3,
+    5, 5
+  ],
+  aboveAverageSentences: 6,
+  belowAverageSentences: 9,
+  averageSentenceLength: 13.578947368421053,
+  averageParagraphLength: 4.75
+}
+
+ZoBro23(D:\Documents\My Stuff\Homeschool\English\From Structure to Style\writing-stats)--> node index.js
+{
+  wordCount: 666, // total word count
+  characterCount: 2830, // alphabets and number only
+  rawCharacterCount: 3642, // all characters
+  sentenceCount: 42, // total sentence count
+  paragraphCount: 7, // total paragraph count
+  longestSentence: 40, // number of words in the longest sentence
+  shortestSentence: 2, // number of words in the shortest sentence
+  wordCountsPerSentence: [ // number of words in each sentence
+    23, 10, 24, 40, 23,  3,  2,  3, 12,  2,
+    21, 12, 19, 21, 17, 28,  5, 23, 19, 16,
+     2, 20, 27, 13, 22, 22, 19, 23, 20,  6,
+    11, 20, 20,  3, 19, 11, 21, 18, 15,  9,
+     5, 17
+  ],
+  longestParagraph: 8, // number of sentences in the longest paragraph
+  shortestParagraph: 3, // number of sentences in the shortest paragraph
+  sentenceCountsPerParagraph: [ // number of sentences in each paragraph
+    4, 7, 3, 6,
+    7, 8, 7
+  ],
+  aboveAverageSentences: 3, // number of sentences with word count more than the average - with buffer
+  belowAverageSentences: 11, // number of sentences with word count less than the average - with buffer
+  averageSentenceLength: 15.857142857142858, // average sentence length in number of words
+  averageParagraphLength: 6 // average paragraph length in number of sentences
+}
+```
---
+++ index.js
@@ -0,0 +1,114 @@
+function writingStats(inputFile, inputLineCount, inputAboveAverageBuffer, inputBelowAverageBuffer, inputSentenceEndCharacters, inputParagraphEndCharacter) {
+    const fs = require('fs')
+    if (typeof inputFile !== 'string') {
+        throw new Error('Parameter `inputFile` must be of type string!')
+    }
+    file = fs.readFileSync(inputFile, 'utf8')
+    const validCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    const sentenceEndCharacters = inputSentenceEndCharacters || ['.', '?', '!']
+    const paragraphEndCharacter = inputParagraphEndCharacter || '\n'
+    const lineCount = inputLineCount || 2 // Number of newline characters per paragraph
+    const aboveAverageBuffer = inputAboveAverageBuffer || 10
+    const belowAverageBuffer = inputBelowAverageBuffer || 5
+
+    let wordCount = 0
+    let characterCount = 0
+    let rawCharacterCount = file.length
+    let sentenceCount = 0
+    let paragraphCount = 0
+    let longestSentence = 0
+    let shortestSentence = file.length
+    let wordCountsPerSentence = []
+    let longestParagraph = 0
+    let shortestParagraph = file.length
+    let sentenceCountsPerParagraph = []
+    let currentWordCount = 0
+    let currentSentenceCount = 0
+
+    for (i = 0; i < file.length; i++) {
+        const character = file.charAt(i).toLowerCase()
+        while (file.charAt(file.length - 1) == paragraphEndCharacter) {
+            file = file.slice(0, file.length - 1)
+        }
+        if (validCharacters.includes(character)) {
+            characterCount++
+        }
+        if (character == ' ') {
+            if (!sentenceEndCharacters.includes(file.charAt(i - 1))) {
+                wordCount++
+                currentWordCount++
+            }
+        }
+        if (sentenceEndCharacters.includes(character)) {
+            wordCount++
+            currentWordCount++
+            sentenceCount++
+            currentSentenceCount++
+            longestSentence < currentWordCount ? longestSentence = currentWordCount : longestSentence = longestSentence
+            shortestSentence > currentWordCount ? shortestSentence = currentWordCount : shortestSentence = shortestSentence
+            wordCountsPerSentence.push(currentWordCount)
+            currentWordCount = 0
+        }
+        if (character == paragraphEndCharacter) {
+            paragraphCount = paragraphCount + (1 / lineCount)
+            if (paragraphCount % 1 == (1 / lineCount)) {
+                longestParagraph < currentSentenceCount ? longestParagraph = currentSentenceCount : longestParagraph = longestParagraph
+                shortestParagraph > currentSentenceCount ? shortestParagraph = currentSentenceCount : shortestParagraph = shortestParagraph
+                sentenceCountsPerParagraph.push(currentSentenceCount)
+            }
+            currentSentenceCount = 0
+        } else if (i == file.length - 1 && character !== '\r') {
+            paragraphCount++
+            longestParagraph < currentSentenceCount ? longestParagraph = currentSentenceCount : longestParagraph = longestParagraph
+            shortestParagraph > currentSentenceCount ? shortestParagraph = currentSentenceCount : shortestParagraph = shortestParagraph
+            sentenceCountsPerParagraph.push(currentSentenceCount)
+            currentSentenceCount = 0
+        }
+    }
+
+    let aboveAverageSentences = 0
+    let belowAverageSentences = 0
+    let averageSentenceLength = 0
+    let averageParagraphLength = 0
+
+    wordCountsPerSentence.forEach(count => {
+        averageSentenceLength = averageSentenceLength + count
+    })
+    averageSentenceLength = averageSentenceLength / wordCountsPerSentence.length
+
+    sentenceCountsPerParagraph.forEach(count => {
+        averageParagraphLength = averageParagraphLength + count
+    })
+    averageParagraphLength = averageParagraphLength / sentenceCountsPerParagraph.length
+
+    wordCountsPerSentence.forEach(count => {
+        if (count - averageSentenceLength > aboveAverageBuffer) {
+            aboveAverageSentences++
+        }
+    })
+
+    wordCountsPerSentence.forEach(count => {
+        if (averageSentenceLength - count > belowAverageBuffer) {
+            belowAverageSentences++
+        }
+    })
+    return {
+        wordCount,
+        characterCount,
+        rawCharacterCount,
+        sentenceCount,
+        paragraphCount,
+        longestSentence,
+        shortestSentence,
+        wordCountsPerSentence,
+        longestParagraph,
+        shortestParagraph,
+        sentenceCountsPerParagraph,
+        aboveAverageSentences,
+        belowAverageSentences,
+        averageSentenceLength,
+        averageParagraphLength
+    }
+}
+
+module.exports = writingStats
